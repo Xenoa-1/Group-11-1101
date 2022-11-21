5634c4e31f06139f43913ef820dbe634381e8fc2
Xander Barajas <xander.barajas01@utrgv.edu>
2022-11-21 13:31:13 -0600

    YEAHHHHHHHH

---
+++ games-data/sandbox-crud-operations-query.sql
@@ -0,0 +1,7 @@
+SELECT * FROM sandbox;
+
+--INSERT INTO  sandbox(int32_value, bool_value, double_value) VALUES (1341588, 0, 333.23456)
+
+--UPDATE sandbox SET int64_value = 21548 WHERE id = 25;
+
+--DELETE FROM sandbox WHERE id = 5;
